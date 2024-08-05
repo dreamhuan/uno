b730ef1eb0bae820914575069d75025fd73ef815
Uniquec <837046740@qq.com>
2024-08-05 23:13:12 +0800

    feat: 主界面UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,55 @@
-import { useState } from 'react'
-import cx from 'classnames'
-import { Button } from 'antd'
-import Time from './components/Time'
-import CardList from './components/CardList'
-import CardItem from './components/CardItem'
-import { Game } from './core/entity/Game'
-import { User } from './core/entity/User'
-import styles from './App.module.scss'
+import { useState } from "react";
+import cx from "classnames";
+import { RedoOutlined, UndoOutlined } from "@ant-design/icons";
+import Time from "./components/Time";
+import CardList from "./components/CardList";
+import CardItem from "./components/CardItem";
+import Operations from "./components/Operations";
+import { Game } from "./core/entity/Game";
+import { User } from "./core/entity/User";
+import styles from "./App.module.scss";
+import { ETurn } from "./core/entity/common";
 
-const game = new Game(4, 7)
-const user1 = new User('user1', 'xxx')
-const user2 = new User('user2', 'xxx')
-const user3 = new User('user3', 'xxx')
-const user4 = new User('user4', 'xxx')
-game.addUser(user1)
-game.addUser(user2)
-game.addUser(user3)
-game.addUser(user4)
-game.init()
-console.log(game)
+const game = new Game(4, 7);
+const user1 = new User("user1", "xxx");
+const user2 = new User("user2", "xxx");
+const user3 = new User("user3", "xxx");
+const user4 = new User("user4", "xxx");
+game.addUser(user1);
+game.addUser(user2);
+game.addUser(user3);
+game.addUser(user4);
+game.init();
+console.log(game);
 function App() {
-  const [_, forceRender] = useState(0)
+  const [_, forceRender] = useState(0);
   return (
     <div className={cx(styles.Game, styles.Container)}>
-      <Time />
-      {game.users.map((user) => (
-        <div>
-          <div>{user.name}</div>
-          <CardList key={user.id} user={user} />
+      <div className={styles.GameTips}>
+        <div className={styles.CardsOrder}>
+          <span>出牌顺序</span>
+          {game.currentTurn === ETurn.CCW ? <UndoOutlined />  : <RedoOutlined />}
         </div>
-      ))}
-      <div>上一家出牌：</div>
-      {game.prevCard && <CardItem card={game.prevCard} />}
-      <div>已出牌列表：</div>
-      <div style={{ display: 'flex', flexWrap: 'wrap' }}>
-        {game.alreadyCards &&
-          game.alreadyCards.map((card) => <CardItem card={card} />)}
       </div>
-      <Button
-        onClick={() => {
-          game.nextTurn()
-          forceRender((prev) => prev + 1)
-        }}
-      >
-        下一轮
-      </Button>
+      <div className={styles.topUser}></div>
+      <div className={styles.CurrentCard}>
+        <div>当前出牌</div>
+        <CardItem card={game.users[0].cards[0]} />
+        <div>
+          <span>倒计时</span>
+          <Time />
+        </div>
+      </div>
+
+      <div className={styles.OperateBtns}>
+        <Operations />
+      </div>
+
+      <div className={styles.UserCardList}>
+        <CardList user={game.users[0]} />
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
