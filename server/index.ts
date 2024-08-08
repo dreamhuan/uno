f0de1594c07c35a1b42dc614e58fc4b0724fd7aa
dreamhuan <fu_kaiqi@qq.com>
2024-08-08 19:55:36 +0800

    feat: 把服务端的逻辑代码合并到客户端仓库

---
+++ server/index.ts
@@ -0,0 +1,156 @@
+import * as path from 'node:path'
+import * as http from 'node:http'
+import * as Koa from 'koa'
+import * as KoaStatic from 'koa-static'
+import { WebSocket } from 'ws'
+import { router } from './router'
+import { Game } from '../src/core/entity/Game'
+import { User } from '../src/core/entity/User'
+
+const app = new Koa()
+app
+  .use(KoaStatic(path.join(__dirname, 'public')))
+  .use(router.routes())
+  .use(router.allowedMethods())
+
+const server = http.createServer(app.callback())
+const wss = new WebSocket.Server({ server })
+
+const userList = [] as string[]
+const userMap = {} as Record<string, any>
+
+let globalGame: Game
+const startGame = () => {
+  const GAME = new Game(4, 7)
+  Object.keys(userMap).forEach((key) => {
+    const userConf = userMap[key]
+    const user = new User(userConf.id, userConf.name, 'xxx')
+    GAME.addUser(user)
+  })
+  GAME.init()
+  console.log('start game')
+  globalGame = GAME
+  return GAME
+}
+
+const getGameDataByUserId = (game?: Game, userId?: string) => {
+  const user = game?.users.find((user) => user.id === userId)
+  if (!user) return null
+  let prevUser
+  if (game?.prevUser) {
+    prevUser = {
+      id: game.prevUser.id,
+      name: game.prevUser.name,
+      icon: game.prevUser.icon,
+      cards: game.prevUser.cards,
+    }
+  }
+  return {
+    userId: user.id,
+    userList,
+    cardList: user.cards,
+    currentTurn: game?.currentTurn,
+    currentColor: game?.currentColor,
+    currentPattern: game?.currentPattern,
+    currentNum: game?.currentNum,
+    currentUserIdx: game?.currentUserIdx,
+    currentUserId: game?.users[game?.currentUserIdx].id,
+    prevCard: game?.prevCard,
+    needAddCardNum: game?.needAddCardNum,
+    users: game?.users.map((u) => {
+      return {
+        id: u.id,
+        name: u.name,
+        icon: u.icon,
+        // cards: u.id === user.id ? u.cards : [],
+        cards: u.cards,
+      }
+    }),
+    alreadyCards: game?.alreadyCards,
+    prevUser,
+    isGetCard: game?.isGetCard,
+  }
+}
+type S = WebSocket & { id: string }
+wss.on('connection', (ws: S) => {
+  console.log('A new client connected!')
+
+  ws.on('message', (buffer) => {
+    const message = buffer.toString()
+    console.log('Received:', message)
+    const data = JSON.parse(message)
+    let game: Game
+    switch (data.type) {
+      case 'open': {
+        const id = data.id
+        ws.id = id
+        if (!userList.includes(id)) {
+          userList.push(id)
+          userMap[ws.id] = {
+            id,
+            client: ws,
+            idx: userList.length - 1,
+          }
+        } else {
+          userMap[ws.id].client = ws
+        }
+        if (userList.length === 4) {
+          game = globalGame || startGame()
+          game.users.forEach((user) => {
+            userMap[user.id].client.send(
+              JSON.stringify({
+                type: 'start',
+                data: getGameDataByUserId(game, user.id),
+              })
+            )
+          })
+        }
+        break
+      }
+      case 'init':
+        userMap[ws.id].name = data.name
+        wss.clients.forEach((client) => {
+          if (client.readyState === WebSocket.OPEN) {
+            client.send(
+              JSON.stringify({
+                type: 'init',
+                data: {
+                  userIdList: userList,
+                },
+              })
+            )
+          }
+        })
+        break
+      case 'action': {
+        console.log(data)
+        const { userId, cardIdx, curColor } = data.data
+        const curUser = globalGame.users[globalGame.currentUserIdx]
+        if (curUser.id === userId) {
+          globalGame.nextTurn(cardIdx, curColor)
+          globalGame.users.forEach((user) => {
+            userMap[user.id].client.send(
+              JSON.stringify({
+                type: 'start',
+                data: getGameDataByUserId(globalGame, user.id),
+              })
+            )
+          })
+        }
+        break
+      }
+    }
+
+    // console.log(userMap)
+    // console.log(userList)
+  })
+
+  ws.on('close', () => {
+    console.log('Client disconnected')
+  })
+})
+
+const port = 3000
+server.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`)
+})
